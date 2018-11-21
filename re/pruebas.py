61b6e122c95fa34cf8e7bb9f7c5b1cc2e5624288
Miguel Angel H <miguelangelutp@gmail.com>
2018-11-20 19:26:47 -0500

    Add more tests

--- re/pruebas.py
+++ re/pruebas.py
@@ -2,4 +2,5 @@ test = [
     'juegos de 1990?',
     'obtener juegos de 2000?',
     'obtener juegos entre 2000 y 2018?',
-    'listar juegos de 1990 a 1996?']
+    'listar juegos de 1990 a 1996?',
+    'dame los juegos desde 2017 hasta 2018?']
